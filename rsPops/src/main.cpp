d8558276626972f73bca383acf1aae945fa14063
Fred Smalley <fred926@nmsu.edu>
2015-05-04 09:55:41 -0600

    Initial commit

---
+++ rsPops/src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  rsPops
+//
+//  Created by Fred Smalley on 5/4/15.
+//  Copyright (c) 2015 Song Lab. All rights reserved.
+//
+
+#include <iostream>
+#include "main.h"
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
